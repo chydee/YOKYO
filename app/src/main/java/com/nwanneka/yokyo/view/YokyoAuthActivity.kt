3840d3acdb11cda41e7a2680073f51b7f9744932
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2022-05-03 10:13:10 +0100

    create nav graph for auth activity

--- app/src/main/java/com/nwanneka/yokyo/view/YokyoAuthActivity.kt
+++ app/src/main/java/com/nwanneka/yokyo/view/YokyoAuthActivity.kt
@@ -2,11 +2,24 @@ package com.nwanneka.yokyo.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.nwanneka.yokyo.R
+import com.nwanneka.yokyo.databinding.ActivityYokyoAuthBinding
 
 class YokyoAuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityYokyoAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_yokyo_auth)
+        binding = ActivityYokyoAuthBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
